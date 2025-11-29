781bef7fd027b97aa67536bb0e7f88c7fa660ee5
jeyosman01 <jey_osman@hotmail.com>
2025-11-29 17:20:36 -0500

    Chapter 1 and 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+
+/* 
+* Default Home Page Component of the Application
+* @ returns
+*/
 export default function Home() {
   return (
     <div>
